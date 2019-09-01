2fe3bc927be5218a5c5734cc1518624157a68a39
Victor Lucas <victorlucas@alu.ufc.br>
2019-09-01 08:33:43 -0300

    Export of sidenav component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 import Navbar from './Navbar.vue';
 import Footer from './Footer.vue';
+import Sidenav from './Sidenav.vue'
+import SidenavLink from './SidenavLink.vue'
 
 export const NavbarIntricately = Navbar;
 export const FooterIntricately = Footer;
+export const SidenavIntricately = Sidenav;
+export const SidenavLinkIntricately = SidenavLink;
