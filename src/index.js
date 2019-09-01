67a940df2a666fc20d8269d8b443ce4f9105aeba
Victor Lucas <victorlucas@alu.ufc.br>
2019-09-01 08:58:41 -0300

    Use of vuex store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
+import router from 'Router'
+import App from './App.vue'
 import 'Styles/intricately.scss';
-
+import store from './store';
 import { NavbarIntricately, FooterIntricately, SidenavIntricately, SidenavLinkIntricately } from './components';
 
 Vue.component('navbar-intricately', NavbarIntricately);
@@ -8,12 +10,13 @@ Vue.component('footer-intricately', FooterIntricately);
 Vue.component('sidenav-intricately', SidenavIntricately);
 Vue.component('sidenavlink-intricately', SidenavLinkIntricately);
 
-import App from './App.vue'
 
-import router from 'Router'
+
+
 
 new Vue({
   render: createElement => createElement(App),
   el: '#app',
   router,
+  store,
 }).$mount('#app');
