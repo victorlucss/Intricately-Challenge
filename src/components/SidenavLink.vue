e8789f5ad027282dde547f2923e5d7394c4260bc
Victor Lucas <victorlucas@alu.ufc.br>
2019-09-01 08:32:32 -0300

    Sidenav link (items of sidenav)

---
+++ src/components/SidenavLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <a href="#" :class="{'active': active}">
+    <li>
+      <slot></slot>
+    </li>
+  </a>
+</template>
+
+<script>
+  export default {
+    name: 'Sidenav-Link',
+    props: ['link', 'active'],
+    
+    data() {
+      return {
+        linkRouter: this.link
+      }
+    }
+  }
+</script>
