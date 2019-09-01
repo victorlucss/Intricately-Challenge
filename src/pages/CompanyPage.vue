54e584cb7149ca51e58fdaf0fea501d6cb7bcf8c
Victor Lucas <victorlucas@alu.ufc.br>
2019-09-01 16:58:16 -0300

    Company page with elements

--- src/pages/CompanyPage.vue
+++ src/pages/CompanyPage.vue
@@ -1,5 +1,155 @@
 <template>
   <div>
-    At microsoft our mission is to empower every person and every organization on the planet to achieve more. Our mission is grounded in both the world in which we live and the future we strive to create. View more.
+    <div class="company-info">
+      <div class="badge">
+        <img src="../assets/microsoft.png" alt="Microsoft Logo" />
+      </div>
+
+      <div class="info">
+        <h1>Microsoft</h1>
+        <span class="location">Redmond, WA USA</span>
+      </div>
+    </div>
+
+    <div class="company-description">
+      At microsoft, our mission is to empower every person and every organization on the planet to achieve more. Our mission is grounded in both the world in which we live and the future we strive to create. <a href="#">View more...</a>
+    </div>
+
+    <div class="column">
+      <div class="row">
+        <card-intricately title="Activities">
+          <div class="activities">
+
+            <ul>
+              <li>
+                <span class="time">08 days ago</span>
+                <div class="icon video-platform"></div>
+                <div class="description">
+                  <a href="#">Cloud Ochestration</a> replaced <del>Microsoft</del> on <a href="#">1 property</a>
+                </div>
+              </li>
+
+              <li>
+                <span class="time">10 days ago</span>
+                <div class="icon content-delivery"></div>
+                <div class="description">
+                  <a href="#">Express Scripts</a> replaced <del>Microsoft</del> on <a href="#">1 property</a>
+                </div>
+              </li>
+
+              <li>
+                <span class="time">12 days ago</span>
+                <div class="icon waf-d-dos-security"></div>
+                <div class="description">
+                  <a href="#">Kaiser Permanente</a> replaced <del>Microsoft</del> on <a href="#">1 property</a>
+                </div>
+              </li>
+
+              <li>
+                <span class="time">14 days ago</span>
+                <div class="icon traffic-management"></div>
+                <div class="description">
+                  <a href="#">Molina Healthcare</a> replaced <del>Microsoft</del> on <a href="#">1 property</a>
+                </div>
+              </li>
+
+              <li>
+                <span class="time">15 days ago</span>
+                <div class="icon dns"></div>
+                <div class="description">
+                  <a href="#">Express Scripts</a> replaced <del>Microsoft</del> on <a href="#">1 property</a>
+                </div>
+              </li>
+
+              <li>
+                <span class="time">16 days ago</span>
+                <div class="icon performance-monitoring"></div>
+                <div class="description">
+                  <a href="#">Kaiser Permanente</a> replaced <del>Microsoft</del> on <a href="#">1 property</a>
+                </div>
+              </li>
+
+              <li>
+                <span class="time">17 days ago</span>
+                <div class="icon saas"></div>
+                <div class="description">
+                  <a href="#">Molina Healthcare</a> replaced <del>Microsoft</del> on <a href="#">1 property</a>
+                </div>
+              </li>
+
+              <li>
+                <span class="time">18 days ago</span>
+                <div class="icon content-delivery"></div>
+                <div class="description">
+                  Microsoft launched M-Cloud
+                </div>
+              </li>
+            </ul>
+
+          </div>
+        </card-intricately>
+      </div>
+
+      <div class="row">
+        <card-intricately title="Similar companies">
+          <div class="company-info-card">
+            <div class="company-info-badge">
+              <div class="badge">
+                <img src="../assets/aws.png" alt="Amazon Web Services Logo">
+              </div>
+
+              <div class="info">
+                <a href="#">Amazon Web Services (AWS)</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="company-info-card">
+            <div class="company-info-badge">
+              <div class="badge">
+                <img src="../assets/oracle.png" alt="Oracle Corporation Logo">
+              </div>
+
+              <div class="info">
+                <a href="#">Oracle Corporation</a>
+              </div>
+            </div>
+          </div>
+        </card-intricately>
+
+        <card-intricately title="Company spend history"></card-intricately>
+
+        <card-intricately title="Company spend history">
+
+          <div class="company-info-card">
+            <div class="company-info-badge">
+              <div class="badge">
+                <img src="../assets/gcloud.png" alt="Google Cloud Logo">
+              </div>
+
+              <div class="info">
+                <a href="#">Google Cloud Platform,</a>
+                <span>Microsoft spend on product $500,000</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="company-info-card">
+            <div class="company-info-badge">
+              <div class="badge">
+                <img src="../assets/centurylink.png" alt="Century Link CDN Logo">
+              </div>
+
+              <div class="info">
+                <a href="#">Century Link CDN,</a>
+                <span>Microsoft spend on product $300,000</span>
+              </div>
+            </div>
+          </div>
+
+
+        </card-intricately>
+      </div>
+    </div>
   </div>
 </template>
