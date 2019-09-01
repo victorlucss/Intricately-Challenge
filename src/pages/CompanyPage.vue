07fbaa9088e78f8cb64b5aaacc9cda663a9d1123
Victor Lucas <victorlucas@alu.ufc.br>
2019-09-01 08:58:34 -0300

    Send the id of sidenavlink to the component

--- src/pages/CompanyPage.vue
+++ src/pages/CompanyPage.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <navbar-intricately></navbar-intricately>
+<br /><br />
+    <sidenav-intricately>
+      <sidenavlink-intricately link="/" id="0">Company data</sidenavlink-intricately>
+      <sidenavlink-intricately link="/" id="1">Company table</sidenavlink-intricately>
+      <sidenavlink-intricately link="/" id="2">Company page</sidenavlink-intricately>
+    </sidenav-intricately>
+<br /><br />
     <footer-intricately></footer-intricately>
   </div>
 </template>
+
+<script>
+  export default {
+  }
+</script>
