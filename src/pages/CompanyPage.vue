2eb88d70ba47b4a2d3139651e0413c09bf21a5df
Victor Lucas <victorlucas@alu.ufc.br>
2019-09-01 09:10:42 -0300

    Use of page structure

--- src/pages/CompanyPage.vue
+++ src/pages/CompanyPage.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
+  <div class="container">
     <navbar-intricately></navbar-intricately>
-<br /><br />
-    <sidenav-intricately>
-      <sidenavlink-intricately link="/" id="0">Company data</sidenavlink-intricately>
-      <sidenavlink-intricately link="/" id="1">Company table</sidenavlink-intricately>
-      <sidenavlink-intricately link="/" id="2">Company page</sidenavlink-intricately>
-    </sidenav-intricately>
-<br /><br />
+
+    <div class="content">
+      <sidenav-intricately>
+        <sidenavlink-intricately link="/" id="0">Company data</sidenavlink-intricately>
+        <sidenavlink-intricately link="/" id="1">Company table</sidenavlink-intricately>
+        <sidenavlink-intricately link="/" id="2">Company page</sidenavlink-intricately>
+      </sidenav-intricately>
+    </div>
+
     <footer-intricately></footer-intricately>
   </div>
 </template>
