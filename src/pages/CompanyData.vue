56ba6837033b048e038c540f712ec764652f460e
Victor Lucas <victorlucas@alu.ufc.br>
2019-09-02 16:09:44 -0300

    Company data page

---
+++ src/pages/CompanyData.vue
@@ -0,0 +1,23 @@
+<template>
+  <card-intricately id="companyData">
+    <div class="wrapper">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tristique pretium cursus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec quis est eu dui consequat aliquam.
+      
+      <input-intricately type="text" required="true" label="Company name" placeholder="e.g. Your company name" />
+      <input-intricately type="currency" min="150" required="true" label="Company spend" placeholder="e.g. $150,000" />
+      <input-intricately type="currency" min="150" max="330" required="true" label="Company spend ability" placeholder="e.g. $150,000 - $330,000" />
+      <input-intricately type="textarea" required="true" label="Notes" placeholder="e.g. Good Tech Company" fullwidth="true" @click.native="openModal()" />
+    </div>
+  </card-intricately>
+</template>
+
+<script>
+  export default {
+    methods: {
+      openModal(){
+        console.log("oie")
+      }
+    }
+    
+  }
+</script>
